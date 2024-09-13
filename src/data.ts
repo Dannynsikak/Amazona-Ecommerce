c108f17bb4cf0d18365dc56cdfcf50d5df87bf30
Dannynsikak <nsikakdanny11@gmail.com>
2024-09-13 16:57:03 +0100

    added some components for the startup

---
+++ src/data.ts
@@ -0,0 +1,53 @@
+import { Frame1, Frame2, Frame3, Frame4 } from "./imgs/index";
+import { Product } from "./types/Product";
+
+export const sampleProducts: Product[] = [
+  {
+    name: "Nike Slim shirt",
+    slug: "nike-slim-shirt",
+    image: Frame1,
+    category: "Shirts",
+    brand: "Nike",
+    price: 120,
+    countInStock: 10,
+    description: "high quality shirt",
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    name: "Adidas Fit Shirt",
+    slug: "adidas-fit-shirt",
+    image: Frame2,
+    category: "Shirts",
+    brand: "Adidas",
+    price: 100,
+    countInStock: 20,
+    description: "high quality product",
+    rating: 4.0,
+    numReviews: 10,
+  },
+  {
+    name: "Lacoste Free Pants",
+    slug: "lacoste-free-pants",
+    image: Frame3,
+    category: "Pants",
+    brand: "Lacoste",
+    price: 220,
+    countInStock: 0,
+    description: "high quality material",
+    rating: 4.7,
+    numReviews: 17,
+  },
+  {
+    name: "Nike SLim Pant",
+    slug: "nike-slim-pant",
+    image: Frame4,
+    category: "Pants",
+    brand: "Nike",
+    price: 78,
+    countInStock: 15,
+    description: "high quality material",
+    rating: 4.5,
+    numReviews: 14,
+  },
+];
